9759156c992f6555defd7a9e49cf9704710dffcd
Daniel <dpere381@fiu.edu>
2021-09-25 16:28:35 -0400

    Added procfile

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 
+require_once "../vendor/autoload.php";
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Twilio\TwiML\MessagingResponse;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,6 +19,16 @@ use Illuminate\Support\Facades\Route;
 // Goal is to send a text to a bot then have it return a respose
 // problem is how do i send a text message to an API
 
+Route::get('/listen',function(){
+  // Set the content-type to XML to send back TwiML from the PHP Helper Library
+  header("content-type: text/xml");
+
+  $response = new MessagingResponse();
+  $response->message(
+      "I'm using the Twilio PHP library to respond to this SMS!"
+  );
+  echo $response;
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
