e09023cfca633f291806762bd94efb1d5e9a09fd
Daniel <dpere381@fiu.edu>
2021-09-25 17:35:38 -0400

    Echo text

--- routes/api.php
+++ routes/api.php
@@ -19,13 +19,13 @@ use Twilio\TwiML\MessagingResponse;
 // Goal is to send a text to a bot then have it return a respose
 // problem is how do i send a text message to an API
 
-Route::post('/listen',function(){
+Route::post('/listen',function(Request $request){
   // Set the content-type to XML to send back TwiML from the PHP Helper Library
   header("content-type: text/xml");
-
+  // message is the text to return to the user
   $response = new MessagingResponse();
   $response->message(
-      "I'm using the Twilio PHP library to respond to this SMS!"
+      $request->Body
   );
   echo $response;
 });
